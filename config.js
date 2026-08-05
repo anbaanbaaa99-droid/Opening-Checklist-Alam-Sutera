eaa752d68acc41046041562e64fdb764886a37d8
anbaanbaaa99-droid <anbaanbaaa99@gmail.com>
2026-08-05 10:26:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.OPENING_APP_CONFIG = {
+window.CHECKLIST_APP_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbxWo38P98PGMVhUngazJgNQdwjw7gSysn-IZHeWrlW6vUKoWrtg7yp78oZ_ImB6a180/exec",
   storeName: "Alam Sutera",
-  appVersion: "1.0.0"
+  appVersion: "2.0.0"
 };
